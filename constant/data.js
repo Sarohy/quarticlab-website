59ec2bd21b215e8e8b318c6147263e0ab2101efc
Abdul Rehman Sorohy <sarohy82@zweidevs.com>
2020-08-31 10:48:31 +0500

    Populate Data

--- constant/data.js
+++ constant/data.js
@@ -1,90 +1,99 @@
 export const services = [
-    {
-        'title':'Mobile App Development',
-        'icon':'/shout.svg',
-        'icon_desc':'',
-        'desc':'We develop sleek looking native and hybrid mobile apps for iOS & Android to ensure the customer satisfaction and  performance at the core.'
-    },
-    {
-        'title':'UI/UX Design',
-        'icon':'/growth.svg',
-        'icon_desc':'',
-        'desc':'Our team is trained to solve problems and provide innovative solutions by following an entire process of UI/UX development.'
-    },
-    {
-        'title':'IOT',
-        'icon':'/report.svg',
-        'icon_desc':'',
-        'desc':'We are experts in building embedded systems with integrated sensors, and wired and wireless communication with mobile and web apps.'
-    },
-    {
-        'title':'Web Development',
-        'icon':'/report.svg',
-        'icon_desc':'',
-        'desc':'We are your creative web development  team, who aim to  leverage the latest technological advances with thoughtful design and serious engineering to build tailored solutions for any industry.'
-    },
-    {
-        'title':'DevOps',
-        'icon':'/report.svg',
-        'icon_desc':'',
-        'desc':'Looking for certified AWS/Google Cloud engineers? Here we are!!'
-    },
-    {
-        'title':'E-Commerce',
-        'icon':'/report.svg',
-        'icon_desc':'',
-        'desc':'We are here to help you in increasing the  global reach of your product or business, by smoothly transitioning  your offline stores to global web.'
-    }
+  {
+    title: "Mobile App Development",
+    icon: "/shout.svg",
+    icon_desc: "",
+    desc:
+      "We develop sleek looking native and hybrid mobile apps for iOS & Android to ensure the customer satisfaction and  performance at the core.",
+  },
+  {
+    title: "UI/UX Design",
+    icon: "/growth.svg",
+    icon_desc: "",
+    desc:
+      "Our team is trained to solve problems and provide innovative solutions by following an entire process of UI/UX development.",
+  },
+  {
+    title: "IOT",
+    icon: "/report.svg",
+    icon_desc: "",
+    desc:
+      "We are experts in building embedded systems with integrated sensors, and wired and wireless communication with mobile and web apps.",
+  },
+  {
+    title: "Web Development",
+    icon: "/report.svg",
+    icon_desc: "",
+    desc:
+      "We are your creative web development  team, who aim to  leverage the latest technological advances with thoughtful design and serious engineering to build tailored solutions for any industry.",
+  },
+  {
+    title: "DevOps",
+    icon: "/report.svg",
+    icon_desc: "",
+    desc: "Looking for certified AWS/Google Cloud engineers? Here we are!!",
+  },
+  {
+    title: "E-Commerce",
+    icon: "/report.svg",
+    icon_desc: "",
+    desc:
+      "We are here to help you in increasing the  global reach of your product or business, by smoothly transitioning  your offline stores to global web.",
+  },
 ];
 
 export const clients = [
-    {
-        'logo':'https://radiustheme.com/demo/wordpress/themes/digeco/wp-content/uploads/2020/03/05_Logo.png',
-        'logo_desc':''
-    },
-    {
-        'logo':'https://radiustheme.com/demo/wordpress/themes/digeco/wp-content/uploads/2020/03/06_Logo.png',
-        'logo_desc':''
-    },
-    {
-        'logo':'https://radiustheme.com/demo/wordpress/themes/digeco/wp-content/uploads/2020/03/04_Logo.png',
-        'logo_desc':''
-    },
-    {
-        'logo':'https://radiustheme.com/demo/wordpress/themes/digeco/wp-content/uploads/2020/03/05_Logo.png',
-        'logo_desc':''
-    },
-]
+  {
+    logo:
+      "https://radiustheme.com/demo/wordpress/themes/digeco/wp-content/uploads/2020/03/05_Logo.png",
+    logo_desc: "",
+  },
+  {
+    logo:
+      "https://radiustheme.com/demo/wordpress/themes/digeco/wp-content/uploads/2020/03/06_Logo.png",
+    logo_desc: "",
+  },
+  {
+    logo:
+      "https://radiustheme.com/demo/wordpress/themes/digeco/wp-content/uploads/2020/03/04_Logo.png",
+    logo_desc: "",
+  },
+  {
+    logo:
+      "https://radiustheme.com/demo/wordpress/themes/digeco/wp-content/uploads/2020/03/05_Logo.png",
+    logo_desc: "",
+  },
+];
 
 export const projects = [
-    {
-        'link':'https://www.shineriteco.com/',
-        'image':'/new 2.png',
-        'name':'ShineRite',
-        'technologies':''
-    },
-    {
-        'link':'https://www.medycle.com/',
-        'image':'/new 3.png',
-        'name':'Medycle',
-        'technologies':''
-    },
-    {
-        'link':'https://www.leoboost.com/',
-        'image':'/new 4.png',
-        'name':'LeoBoost',
-        'technologies':''
-    },
-    {
-        'link':'https://checkin.fortyeight.ai/',
-        'image':'/new 5.png',
-        'name':'Fortyeight',
-        'technologies':''
-    },
-    {
-        'link':'https://www.officersurvey.com/',
-        'image':'/new 6.png',
-        'name':'OfficerSurvey',
-        'technologies':'Django | AWS'
-    }
-]
+  {
+    link: "https://www.shineriteco.com/",
+    image: "/new 2.png",
+    name: "ShineRite",
+    technologies: "Django | React",
+  },
+  {
+    link: "https://www.medycle.com/",
+    image: "/new 3.png",
+    name: "Medycle",
+    technologies: "React Native | Django | Vue",
+  },
+  {
+    link: "https://www.leoboost.com/",
+    image: "/new 4.png",
+    name: "LeoBoost",
+    technologies: "",
+  },
+  {
+    link: "https://checkin.fortyeight.ai/",
+    image: "/new 5.png",
+    name: "Fortyeight",
+    technologies: "",
+  },
+  {
+    link: "https://www.officersurvey.com/",
+    image: "/new 6.png",
+    name: "OfficerSurvey",
+    technologies: "Django | AWS",
+  },
+];
