7a55fde55ab4ea7b3dadd476fa0b51f2a85d3d15
Musaddiq <musaddiqkhan818@gmail.com>
2020-12-22 18:58:29 +0500

    Get a Qoute:Integrate API on Get A Qoute

--- components/button.js
+++ components/button.js
@@ -1,9 +1,16 @@
-import styles from '../styles/Button.module.css'
+import styles from "../styles/Button.module.css";
 
 const Button = (props) => {
-    return(
-        <button type={props.type} className={`btn ${styles.btn_color}`}>{props.text}</button>  
-    );
-}
+  return (
+    <button
+      type={props.type}
+      className={`btn ${styles.btn_color}`}
+      onClick={props.onClick}
+      onSubmit={props.onSubmit}
+    >
+      {props.text}
+    </button>
+  );
+};
 
 export default Button;
