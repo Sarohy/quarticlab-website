5c4b3548976471758edfbfb445a9f2de9c01a92c
Malik Hamza <malikhamza.v@gmail.com>
2022-09-27 16:23:25 +0500

    Home Page Services: Change UI

--- components/serviceCard.js
+++ components/serviceCard.js
@@ -1,18 +1,18 @@
-import Head from 'next/head';
-import styles from '../styles/ServiceCard.module.css'
+import Head from "next/head";
+import styles from "../styles/ServiceCard.module.css";
 
 const ServiceCard = (props) => {
-    return(
-        <div className={styles.card_items}>
-            <div className={styles.card_items_icon}>
-                <img src={props.icon} alt={props.icon_desc} />
-            </div>
-            <div className={styles.card_items_content}>
-                <h3>{props.title}</h3>
-                <p>{props.desc}</p>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={styles.card_items}>
+      <div className={styles.card_items_icon}>
+        <img src={props.icon} alt={props.icon_desc} />
+      </div>
+      <div className={styles.card_items_content}>
+        <h3>{props.title}</h3>
+        <p className={styles.serviceDisc}>{props.desc}</p>
+      </div>
+    </div>
+  );
+};
 
 export default ServiceCard;
