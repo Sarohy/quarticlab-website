492ecd825fa2ec705950cc4fbc234e76883ff181
Ali Baig <ali.baig@zweidevs.com>
2023-04-04 12:21:13 +0500

    MUI Installation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,8 @@
 import { Layout } from "@component/Components";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import "@component/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
