0ecce81b74aa079a95296d51610dff57bd868f72
abdullahqureshi5050 <abdullahqureshi5050@gmail.com>
2023-05-02 10:39:15 +0500

    services page responsive

--- src/Components/CommonComponents/ServicesCard/ServicesCard.jsx
+++ src/Components/CommonComponents/ServicesCard/ServicesCard.jsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 
@@ -5,46 +6,108 @@ const ServicesCard = (props) => {
   const { cardData } = props;
   return (
     <>
-      {cardData.map((element, key) => (
-        <div className="services-card-container">
-          <div className="services-icon-card-container">
-            <Image
-              className="services-icon"
-              src={element.cardIcon}
-              alt="web-dev-icon"
-            />
-            <div className="services-icon-card-title">
-              <div>
-                {element.cardIconTitle.firstLine}
-                <br></br>
-                {element.cardIconTitle.secondLine}
+      <Box
+        sx={{
+          display: {
+            xs: "none",
+            sm: "block",
+            md: "block",
+            lg: "block",
+            xl: "block",
+            xxl: "block",
+          },
+        }}
+      >
+        {cardData.map((element, key) => (
+          <div key={key} className="services-card-container">
+            <Box
+              sx={{
+                display: {
+                  xs: "none",
+                  sm: "none",
+                  md: "flex",
+                  lg: "flex",
+                  xl: "flex",
+                  xxl: "flex",
+                },
+              }}
+            >
+              <div className="services-icon-card-container">
+                <Image
+                  className="services-icon"
+                  src={element.cardIcon}
+                  alt="web-dev-icon"
+                />
+                <div className="services-icon-card-title">
+                  <div>
+                    {element.cardIconTitle.firstLine}
+                    <br></br>
+                    {element.cardIconTitle.secondLine}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="services-card-details-container">
-            <span className="services-card-details-header">
-              {element.cardTitle}
-            </span>
-            <div className="services-card-details">{element.cardDetails} </div>
-            <div className="services-card-footer-container">
-              <span className="services-card-footer-header">
-                {element.footerTitle}:
+            </Box>
+
+            <div className="services-card-details-container">
+              <span className="services-card-details-header">
+                {element.cardTitle}
               </span>
-              <div className="services-footer-images-container">
-                {element.footerImages &&
-                  element.footerImages.map((image, key) => (
-                    <Image
-                      key={key}
-                      className="services-footer-image"
-                      src={image}
-                      alt={"node-icon"}
-                    />
-                  ))}
+              <div className="services-card-details">
+                {element.cardDetails}{" "}
+              </div>
+              <div className="services-card-footer-container">
+                <span className="services-card-footer-header">
+                  {element.footerTitle}:
+                </span>
+                <div className="services-footer-images-container">
+                  {element.footerImages &&
+                    element.footerImages.map((image, key) => (
+                      <Image
+                        key={key}
+                        className="services-footer-image"
+                        src={image}
+                        alt={"node-icon"}
+                      />
+                    ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </Box>
+
+      {/* mobile View */}
+      <Box
+        sx={{
+          display: {
+            xs: "block",
+            sm: "none",
+            md: "none",
+            lg: "none",
+            xl: "none",
+            xxl: "none",
+          },
+        }}
+      >
+        {cardData.map((element, key) => (
+          <Box>
+            <div className="services-icon-card-container-mobile">
+              <Image
+                className="services-icon"
+                src={element.cardIcon}
+                alt="web-dev-icon"
+              />
+              <div className="services-icon-card-title">
+                <div>
+                  {element.cardIconTitle.firstLine}
+                  <br></br>
+                  {element.cardIconTitle.secondLine}
+                </div>
+              </div>
+            </div>
+          </Box>
+        ))}
+      </Box>
     </>
   );
 };
--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -2,7 +2,7 @@ import {
   InstantBookingBanner,
   ServicesCard,
 } from "@component/Components/CommonComponents";
-import { Review1Icon, Review2Icon } from "@component/assets/reviewIcons";
+
 import {
   BlockchainIcon,
   MobileDevIcon,
@@ -42,42 +42,11 @@ import {
   googleCloudIcon,
   goDaddyIcon,
 } from "@component/assets/serviceIcons";
-import { addReview } from "@component/firebase/firebaseRequests";
-import {
-  addProject,
-  addService,
-  getAllProjects,
-  getAllReviews,
-  getAllServices,
-} from "@component/firebase/firebaseRequests";
+
 import React from "react";
-import Image from "next/image";
-
-const projectDetails = {
-  projectCategory: "Salon App",
-  projectImage: "https://i.ibb.co/FbGJDsg/Screenshot-at-Apr-07-02-16-25.png",
-  projectId: 3,
-  projectName: "You Media Pvt.",
-};
-
-const serviceDetail = {
-  serviceDescription:
-    "The UI/UX Design brings a design-centric approach to user interface and user experience.Our team is trained to solve problems and provide innovative solutions by following an entire process of UI/UX development.",
-  serviceIcon: UIUXIcon,
-  serviceId: 3,
-  serviceTitle: "UI/UX Design",
-};
-
-const reviewDetail = {
-  clientDescription:
-    "I found Zweidevs Team very professional and hard working. They just didn't only develop my web app but actually guided me as well through different phases. Thank you will definitely use your services again",
-  clientId: 9,
-  clientImage: Review2Icon,
-  clientName: "Client 9",
-  clientRating: 5,
-};
 
 function Services() {
+  // Services card data
   const cardData = [
     {
       cardIcon: WebDevIcon,
@@ -149,26 +118,10 @@ function Services() {
       footerImages: [AWSIcon, AzureIcon, googleCloudIcon, goDaddyIcon],
     },
   ];
-  const handleGetDb = async () => {
-    // const projects = await getAllProjects();
-    // const services = await getAllServices();
 
-    const reviews = await getAllReviews();
-    console.log("reviews ==> ", reviews);
-  };
-
-  const handleSetDb = async () => {
-    // const response = await addProject(projectDetails);
-    // const response = await addService(serviceDetail);
-    const response = addReview(reviewDetail);
-  };
   return (
     <div className="services-root">
-      <span className="services-header">
-        {" "}
-        Our Services --
-        {/* <span className="services-hr"></span>{" "} */}
-      </span>
+      <span className="services-header"> Our Services --</span>
       <ServicesCard cardData={cardData} />
       <InstantBookingBanner />
     </div>
