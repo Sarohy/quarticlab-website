6c3f687c9ad54f39867c3764fedeaa1bd10143e2
Ali Baig <ali.baig@zweidevs.com>
2023-05-02 23:38:29 +0500

    Package conflicts resolved

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,4 @@
 import { Layout } from "@component/Components";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import '@fontsource/poppins/300.css';
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/500.css';
