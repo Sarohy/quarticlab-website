929890952ce6358609c9d09723fdd312e6eb4432
Sara Khurshid <Sara.Khurshid@zweidevs.com>
2023-05-16 11:05:35 +0500

    package.json: add axios package for api integration BlogCard.jsx: add new properties index.js: api integrate and data set

---
+++ src/pages/api/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+// const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = "http://139.59.37.140:8001/";
+
+const baseInstance = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const getApiWithoutAuth = async (url) => {
+  try {
+    const res = await baseInstance.get(url);
+    return {
+      data: res.data,
+    };
+  } catch (err) {
+    return err.response;
+  }
+};
