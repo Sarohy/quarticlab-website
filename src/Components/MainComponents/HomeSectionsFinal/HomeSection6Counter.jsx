a39c888927f7c081c44ed1094c1c0635dad7f7bf
msaeed140 <134705365+msaeed140@users.noreply.github.com>
2023-06-01 12:43:26 +0500

    why zweidevs section changes

---
+++ src/Components/MainComponents/HomeSectionsFinal/HomeSection6Counter.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState, useRef } from "react";
+import styles from "./HomeSection.module.css";
+
+const HomeSection6Counter = (props) => {
+  const ref = useRef(null);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const { label, number, duration, sign } = props;
+  const [count, setCount] = useState("0");
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    });
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, []);
+
+  useEffect(() => {
+    let start = 0;
+    const end = parseInt(number.substring(0, 3));
+
+    if (start === end) return;
+    let totalMilSecDur = parseInt(duration);
+    let incrementTime = (totalMilSecDur / end) * 1000;
+
+    let timer = setInterval(() => {
+      start += 1;
+      setCount(String(start) + number.substring(3));
+      if (start === end) clearInterval(timer);
+    }, incrementTime);
+  }, [number, duration, isIntersecting]);
+
+  return (
+    <div className="Count" ref={ref}>
+      <div>
+        <span className={styles.sinceyear}>
+          {count}
+          {sign}
+        </span>
+        <br />
+        <span>{label} </span>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSection6Counter;
