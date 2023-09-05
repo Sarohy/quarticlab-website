c2162866c47f0369070f80b9ebad9c4f4ec4fd8b
Umar Arshad <elliotalderson4444@gmail.com>
2023-09-05 12:03:01 +0500

    home page refactoring done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,36 @@
-import { Inter } from "next/font/google";
 import {
-  HomeSection1,
-  HomeSection2,
-  HomeSection3,
-  HomeSection4,
-  HomeSection5,
-  HomeSection6,
-  HomeSection7,
-  HomeSection8,
+    HomeSection1,
+    HomeSection2,
+    HomeSection3,
+    HomeSection4,
+    HomeSection5,
+    HomeSection6,
+    HomeSection7,
+    HomeSection8,
 } from "@component/Components/MainComponents/HomeSectionsFinal";
-
-const inter = Inter({ subsets: ["latin"] });
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const section2 = () => document.getElementById("homeSection2");
-  const section3 = () => document.getElementById("homeSection3");
+    const section2 = () => document.getElementById("homeSection2");
+    // const section3 = () => document.getElementById("homeSection3");
 
-  const handleButtonClickSection1 = () => {
-    section2().scrollIntoView({ behavior: "smooth" });
-  };
+    const handleButtonClickSection1 = () => {
+        section2().scrollIntoView({ behavior: "smooth" });
+    };
 
-  const handleButtonClickSection2 = () => {
-    section3().scrollIntoView({ behavior: "smooth" });
-  };
-  return (
-    <div style={{marginTop:"15vh", backgroundColor:"white"}}> 
-      <HomeSection1 handleButtonClick={handleButtonClickSection1} />
-      <HomeSection2 />
-      <HomeSection3 />
-      <HomeSection4 />
-      <HomeSection5 />
-      <HomeSection6 />
-      <HomeSection7 />
-      <HomeSection8 />
-    </div>
-  );
+    // const handleButtonClickSection2 = () => {
+    //     section3().scrollIntoView({ behavior: "smooth" });
+    // };
+    return (
+        <div className={styles.HPStyle}>
+            <HomeSection1 handleButtonClick={handleButtonClickSection1} />
+            <HomeSection2 />
+            <HomeSection3 />
+            <HomeSection4 />
+            <HomeSection5 />
+            <HomeSection6 />
+            <HomeSection7 />
+            <HomeSection8 />
+        </div>
+    );
 }
