9f83f6c8fc7e03749c50cbfe9919f36bff6a6d9c
Sarohy <abdul.rehman@zweidevs.com>
2026-03-27 01:10:16 +0500

    Update package.json

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,8 +4,11 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head> 
-        <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
+      <Head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
           window.dataLayer = window.dataLayer || [];
